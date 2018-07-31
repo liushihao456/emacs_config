f0b16c9fc7a942e57684fe0bc8ee854e6f3c30d8
liushihao <liushihao@pku.edu.cn>
2018-07-31 11:31:26 +0800

    Switched to use-package

--- elpa/lsp-java-20180701.811/lsp-java-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "lsp-java" "20180701.811" "Java support for lsp-mode" '((emacs "25.1") (lsp-mode "3.0")) :commit "b3d671ccc264b32492e30f48b4d5a52f6b88145d" :keywords '("java") :url "https://github.com/emacs-lsp/lsp-java")
